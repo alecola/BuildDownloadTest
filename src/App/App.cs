8f1eb01119a99cc8443bbb5535a710d1f486dd28
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-12-16 09:44:39 -0500

    code

---
+++ src/App/App.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Bit.App
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new HomePage();
+        }
+    }
+
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            Content = new Label { Text = "Hello" };
+        }
+    }
+}
---
+++ src/iOS.Extension/LoadingViewController.cs
@@ -0,0 +1,22 @@
+using System;
+using Google.Analytics;
+using UIKit;
+
+namespace Bit.iOS.Extension
+{
+    public partial class LoadingViewController : UIViewController
+    {
+        public LoadingViewController(IntPtr handle) : base(handle)
+        { }
+
+        public override void ViewDidLoad()
+        {
+            System.Diagnostics.Debug.WriteLine(
+                "TESTLOG: ClientId is null: " + GaiConstants.ClientId == null);
+            System.Diagnostics.Debug.WriteLine(
+                "TESTLOG: AppId is null: " + GaiConstants.AppId == null);
+
+            base.ViewDidLoad();
+        }
+    }
+}
